aac4ebcd9914183a0573cb84f5bc74293a24992b
Kaustav Biswas <kaustavbiswas1697@gmail.com>
2020-05-29 23:20:51 +0530

    searching for hashtags implemented

--- InstaBot.py
+++ InstaBot.py
@@ -2,16 +2,24 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+#open browser to the url
 browser = webdriver.Edge()
 browser.get('https://www.instagram.com/')
 time.sleep(2)
 
+#click on the explore button
 explore = browser.find_element_by_xpath('//a[@href= "/explore/"')
 explore.click()
 time.sleep(2)
 
+#find the search box and type in the tag
 search = browser.find_element_by_css_selector("input[placeholder='Search']")
-search.send_keys('#inkart').send_keys(Keys.ENTER)
+search.send_keys('#inkart')
+time.sleep(2)
+
+#open the first suggestion
+suggestion1 = browser.find_element_by_xpath('//*[@id="react-root"]/section/nav/div[2]/div/div/div[2]/div[2]/div[2]/div/a[1]')
+suggestion1.click()
 time.sleep(2)
 
 time.sleep(5)
