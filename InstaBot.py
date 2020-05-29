5d838262e8f3e092629c28617b1bfc22d3ca3934
Kaustav Biswas <kaustavbiswas1697@gmail.com>
2020-05-29 22:19:29 +0530

    Opens the explore page and searches tags

--- InstaBot.py
+++ InstaBot.py
@@ -1,8 +1,18 @@
 import time
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 browser = webdriver.Edge()
 browser.get('https://www.instagram.com/')
+time.sleep(2)
+
+explore = browser.find_element_by_xpath('//a[@href= "/explore/"')
+explore.click()
+time.sleep(2)
+
+search = browser.find_element_by_css_selector("input[placeholder='Search']")
+search.send_keys('#inkart').send_keys(Keys.ENTER)
+time.sleep(2)
 
 time.sleep(5)
 
